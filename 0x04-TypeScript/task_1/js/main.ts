bfab13ac57c5ada633b8c93484857291d5a7280c
Rachel Lewis <RLewis11769@gmail.com>
2021-07-22 19:52:19 +0000

    Add task 2 and update readonly of task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,11 +1,14 @@
 // Create object with optional parameters
 
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [key: string]: any;
 }
 
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
