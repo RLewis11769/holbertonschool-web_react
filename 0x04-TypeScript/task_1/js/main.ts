bc5c4eff1a89c972d915b28a83358aabe311bbcc
Rachel Lewis <RLewis11769@gmail.com>
2021-07-22 20:50:36 +0000

    Add task 4 and tidy up other tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,10 @@
 // Create object with optional parameters
+// Create object extending other object
+// Create function with interface
+// Create class with interface and constructor interface
 
 interface Teacher {
+  // Parent interface
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
@@ -10,6 +14,7 @@ interface Teacher {
 }
 
 interface Directors extends Teacher {
+  // Child interface
   numberOfReports: number;
 }
 
@@ -19,5 +24,34 @@ function printTeacher({firstName, lastName}: Teacher) {
 }
 
 interface printTeacherFunction {
+  // Describes required keys/values passed into printTeacher function
+  // Describes return as string
   (firstName: string, lastName: string): string;
+}
+
+interface StudentInterface {
+  // Describes keys/values of instance methods passed into StudentClass class
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentConstructor {
+  // Describes required keys/values passed into StudentClass constructor
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+class StudentClass implements StudentInterface {
+  // Attributes and methods of StudentClass class
+
+  constructor(firstName: string, lastName: string) {}
+
+  workOnHomework() {
+    // Returns string when called
+    return 'Currently working';
+  }
+
+  displayName() {
+    // Returns string when called
+    return this.firstName;
+  }
 }
