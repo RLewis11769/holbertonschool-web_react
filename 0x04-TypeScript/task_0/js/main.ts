9e88102fa6628aef7f5edb47aeda9888ceb8f0da
Rachel Lewis <RLewis11769@gmail.com>
2021-07-28 17:14:23 +0000

    Add table to 0 - can't add thead yet

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -9,23 +9,35 @@ interface Student {
 }
 
 // Requires all fields filled out
-let student1: Student = {
+const student1: Student = {
     firstName: 'Shadow',
     lastName: 'Lewis',
     age: 4,
     location: 'Earth'
 }
 
-let student2: Student = {
+const student2: Student = {
     firstName: 'Vader',
     lastName: 'Lewis',
     age: 3,
     location: 'Mustafar'
 }
 
-let studentsList: Array<Student> = [student1, student2];
+// Array of type Student with student1 and student2 data
+const studentsList: Array<Student> = [student1, student2];
 
-// // Loop below is not part of the task, just my own check if I did it correctly
-// studentsList.forEach(student => {
-//     console.log(student);
-// })
+// Create table
+const table = document.createElement('table');
+// For student1 and student2
+studentsList.forEach(function(student) {
+  // Create row
+  const row = table.insertRow();
+  // Add firstname and lastname cell in row
+  const firstname = row.insertCell();
+  const location = row.insertCell();
+  // Add data from studentList as html
+  firstname.innerHTML = student.firstName;
+  location.innerHTML = student.location;
+  });
+// Add data to table
+document.body.appendChild(table);
