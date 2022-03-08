f18c5dd9566993fb7ea055018c3e60113f2c6c34
Rachel Lewis <RLewis11769@gmail.com>
2022-03-08 14:16:54 -0600

    Add data for task 1 - basic config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+const _ = require('lodash');
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+let count = 0;
+function updateCounter() {
+  // Update count p tag based on button click below
+  $("#count").html(count += 1);
+};
+
+// Won't allow click spamming - one per second
+$('button').on('click', _.debounce(updateCounter, 1000));
