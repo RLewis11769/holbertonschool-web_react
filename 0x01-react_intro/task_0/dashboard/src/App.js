31b9c12a08e5f807d16b9152d219f72a6c3895b8
Rachel Lewis <RLewis11769@gmail.com>
2022-03-09 10:44:30 -0600

    Add specifically-chosen react setup files and files for task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import logo from './holbie-logo.jpg';
+// import icon from './holbie-icon.ico';
+import './App.css';
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>School dashboard</h1>
+        </header>
+        <body className='App-body'>
+          <p>Login to access the full dashboard</p>
+        </body>
+        <footer className='App-footer'>
+          <p>Copyright 2020 - holberton School</p>
+        </footer>
+      </>
+    )
+  }
+}
