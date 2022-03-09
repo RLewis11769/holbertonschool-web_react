4e5f13723073c13a438c41618f008f6d7536b63d
Rachel Lewis <RLewis11769@gmail.com>
2022-03-08 23:01:47 -0600

    Working as previously - haven't finished task

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [  'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 8564,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
