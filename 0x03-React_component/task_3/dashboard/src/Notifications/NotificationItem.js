e8270bad3344cf676a3dddd028d6bc016606ddb5
Rachel Lewis <RLewis11769@gmail.com>
2022-03-21 19:32:18 -0500

    Add BodySection components

---
+++ 0x03-React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotificationItem = function NotificationItem({
+  type, html, value, markAsRead,
+}) {
+  // ESLint attempts - not able to split lines properly
+  if (value) {
+    return (
+      <li data-notification-type={type} onClick={markAsRead}>{value}</li>
+    );
+  }
+  return (
+    <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={markAsRead} />
+  );
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func.isRequired,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  value: null,
+  html: null,
+};
+
+export default NotificationItem;
