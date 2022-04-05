19d203ab5d9c2d0b0dd050372aad24a18917761b
Rachel Lewis <RLewis11769@gmail.com>
2022-04-04 23:30:53 -0500

    Remove console.log for linting

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -5,11 +5,11 @@ export default function getImmutableObject(object) {
   return fromJS(object);
 }
 
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}
+// const obj = {
+//   fear: true,
+//   smell: -1033575916.9145899,
+//   wall: false,
+//   thing: -914767132
+// }
 
-console.log(getImmutableObject(obj));
+// console.log(getImmutableObject(obj));
