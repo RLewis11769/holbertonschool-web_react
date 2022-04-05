785528bda467d197507a1670bdfe392688d387c7
Rachel Lewis <RLewis11769@gmail.com>
2022-04-05 01:33:00 -0500

    Add data for 8-seq.js - seq

---
+++ 0x01-React_Immutable/8-seq.js
@@ -0,0 +1,33 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(obj) {
+  // Seq is lazy operation to efficiently chain operations by not creating intermediate objects
+  const bestStudents = Seq(obj)
+    // Filter to only students with score > 70
+    .filter((student) => student.score > 70)
+    // Map to specific format
+    .map((student) => {
+      return {
+        score: student.score,
+        firstName: student.firstName.slice(0, 1).toUpperCase() + student.firstName.slice(1),
+        lastName: student.lastName.slice(0, 1).toUpperCase() + student.lastName.slice(1),
+      };
+    });
+  // Convert immutable.js Seq to normal JS array
+  console.log(bestStudents.toJS());
+}
+
+// const grades = {
+//   1: {
+//     score: 99,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   2: {
+//     score: 79,
+//     firstName: 'suillaume',
+//     lastName: 'galva',
+//   }
+// };
+
+// printBestStudents(grades);
