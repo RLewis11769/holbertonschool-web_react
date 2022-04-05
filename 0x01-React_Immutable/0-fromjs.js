816d1d069dfe1e387940500c1b32f957626b6a51
Rachel Lewis <RLewis11769@gmail.com>
2022-04-04 23:21:20 -0500

    Export function

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -1,15 +1,15 @@
 const { fromJS } = require('immutable');
 
-function getImmutableObject(object) {
+export default function getImmutableObject(object) {
   // Convert object to immutable map
   return fromJS(object);
 }
 
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}
+// const obj = {
+//   fear: true,
+//   smell: -1033575916.9145899,
+//   wall: false,
+//   thing: -914767132
+// }
 
-console.log(getImmutableObject(obj));
+// console.log(getImmutableObject(obj));
