eff035d09e0be6edde17ec4291e9b5ab9fcfbc41
Rachel Lewis <RLewis11769@gmail.com>
2022-04-05 00:44:28 -0500

    Create 2 immutable maps - one modified from other

---
+++ 0x01-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+// import { withMutations } from 'immutable';
+
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+// Convert index 2 to 'Ethan' and index 3 to 'Mason'
+export const map2 = map.withMutations(idx => {
+  idx.set(2, 'Benjamin');
+  idx.set(4, 'Oliver');
+});
