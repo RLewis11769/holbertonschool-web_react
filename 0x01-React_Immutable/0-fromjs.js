e6db90bf8adbf65f44fdfa570facb1bf2f648779
Rachel Lewis <RLewis11769@gmail.com>
2022-04-04 23:18:00 -0500

    Read directions - fromJS for 0, Map for 1

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -1,15 +1,15 @@
-const { Map } = require('immutable');
+const { fromJS } = require('immutable');
 
 function getImmutableObject(object) {
   // Convert object to immutable map
-  return Map(object);
+  return fromJS(object);
 }
 
-// const obj = {
-//   fear: true,
-//   smell: -1033575916.9145899,
-//   wall: false,
-//   thing: -914767132
-// }
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+}
 
-// console.log(getImmutableObject(obj));
+console.log(getImmutableObject(obj));
---
+++ 0x01-React_Immutable/1-map.js
@@ -0,0 +1,15 @@
+const { Map } = require('immutable');
+
+function getImmutableObject(object) {
+  // Convert object to immutable map
+  return Map(object);
+}
+
+// const obj = {
+//   fear: true,
+//   smell: -1033575916.9145899,
+//   wall: false,
+//   thing: -914767132
+// }
+
+// console.log(getImmutableObject(obj));
